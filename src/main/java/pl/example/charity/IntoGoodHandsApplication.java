05ffa6ecb3d6d723f9bf4c611129956eace44e7b
nixframe <jakub.aussenberg+github@gmail.com>
2020-07-12 23:14:34 +0200

    Added configuration for spring application

---
+++ src/main/java/pl/example/charity/IntoGoodHandsApplication.java
@@ -0,0 +1,13 @@
+package pl.example.charity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntoGoodHandsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntoGoodHandsApplication.class, args);
+    }
+
+}
